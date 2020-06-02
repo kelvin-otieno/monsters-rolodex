b640d7ce9e996076a83f438a21daa0e3cde5e51d
kelvin-otieno <kelvinotieno06@gmail.com>
2020-06-03 02:08:03 +0300

    Updates

---
+++ precache-manifest.cb8f44544a959b28e5f08f0a640fc618.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d73d09cff891735541cbcbed79856f",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bb3e685f13ad72f3a331",
+    "url": "/github.io/monsters-rolodex/static/css/main.0897370d.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/github.io/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3e685f13ad72f3a331",
+    "url": "/github.io/monsters-rolodex/static/js/main.b1ce7005.chunk.js"
+  },
+  {
+    "revision": "4b6696fd3589e037a001",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.7ae4c3c1.js"
+  },
+  {
+    "revision": "e16831b3cf08e47ec3c2c4f660ce2593",
+    "url": "/github.io/monsters-rolodex/static/media/anime1.e16831b3.png"
+  },
+  {
+    "revision": "cf9af5c706164afa856426f3af22add8",
+    "url": "/github.io/monsters-rolodex/static/media/anime10.cf9af5c7.png"
+  },
+  {
+    "revision": "68aafc0981c571fd1546fbab6fcf9a84",
+    "url": "/github.io/monsters-rolodex/static/media/anime2.68aafc09.png"
+  },
+  {
+    "revision": "4e59ef56247fbf6e634ae377f973985e",
+    "url": "/github.io/monsters-rolodex/static/media/anime3.4e59ef56.png"
+  },
+  {
+    "revision": "8cfe202e351c45763711998c140b8e9f",
+    "url": "/github.io/monsters-rolodex/static/media/anime4.8cfe202e.png"
+  },
+  {
+    "revision": "89f22b4cd7134faaea8c42c47c6dbdb2",
+    "url": "/github.io/monsters-rolodex/static/media/anime5.89f22b4c.png"
+  },
+  {
+    "revision": "45a85347313271ec91437ef93b2a039a",
+    "url": "/github.io/monsters-rolodex/static/media/anime6.45a85347.png"
+  },
+  {
+    "revision": "1b97816d7b2f6c471806903d2b73b77d",
+    "url": "/github.io/monsters-rolodex/static/media/anime7.1b97816d.png"
+  },
+  {
+    "revision": "a71d84cd35308913c0e51ff29aa13918",
+    "url": "/github.io/monsters-rolodex/static/media/anime8.a71d84cd.png"
+  },
+  {
+    "revision": "b2db7d09b55fb4bcaf3970d559aad9a4",
+    "url": "/github.io/monsters-rolodex/static/media/anime9.b2db7d09.png"
+  }
+]);
